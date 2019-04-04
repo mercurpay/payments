ff2fc97f4e51d661ef9b6c6523594dac24c7b580
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2019-04-03 22:25:13 -0300

    configure nats properties

--- src/main/java/tech/claudioed/payments/infra/nats/NatsProducer.java
+++ src/main/java/tech/claudioed/payments/infra/nats/NatsProducer.java
@@ -3,34 +3,43 @@ package tech.claudioed.payments.infra.nats;
 import io.nats.client.Connection;
 import io.nats.client.Nats;
 import io.nats.client.Options;
-import java.io.IOException;
 import java.time.Duration;
-import lombok.extern.slf4j.Slf4j;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/** @author claudioed on 2019-03-30. Project payments */
 @Configuration
-@Slf4j
-public class NatsConfiguration {
+public class NatsProducer {
 
-  @Bean
-  public Connection createConnection(
-      @Value("${nats.host}") String host,
-      @Value("${nats.username}") String username,
-      @Value("${nats.password}") String password
-  ) throws IOException, InterruptedException {
-    log.info("Configuring NATS Connection, Host[{}]", host);
+  private final String natsUser;
+
+  private final String natsPass;
+
+  private final String natsHost;
+
+  public NatsProducer(
+      @Value("${nats.user}") String natsUser,
+      @Value("${nats.pass}") String natsPass,
+      @Value("${nats.host}") String natsHost) {
+    this.natsUser = natsUser;
+    this.natsPass = natsPass;
+    this.natsHost = natsHost;
+  }
+
+  @Bean("natsConnection")
+  @SneakyThrows
+  public Connection natsConnection() {
     return Nats.connect(new Options.Builder()
         .connectionTimeout(Duration.ofSeconds(2))
         .pingInterval(Duration.ofSeconds(10))
         .reconnectWait(Duration.ofSeconds(1))
-        .userInfo(username,password)
+        .userInfo(natsUser,natsPass)
         .maxReconnects(-1)
         .reconnectBufferSize(-1)
         .connectionName(System.getenv("HOSTNAME"))
-        .server(host)
+        .server(natsHost)
         .build());
   }
-
 }
